a8e719c24c6a29e74ef846de02c6a3a24f508280
Dmitry Mamonov <dmitry_mamonovv@mail.ru>
2024-04-21 15:51:56 +0300

    Добавьте файлы проекта.

---
+++ 4_лаба_Виндовс_Формам/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace _4_лаба_Виндовс_Формам
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Xn = new System.Windows.Forms.TextBox();
+            this.Xk = new System.Windows.Forms.TextBox();
+            this.a = new System.Windows.Forms.TextBox();
+            this.b = new System.Windows.Forms.TextBox();
+            this.DX = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Res = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Xn
+            // 
+            this.Xn.Location = new System.Drawing.Point(275, 56);
+            this.Xn.Name = "Xn";
+            this.Xn.Size = new System.Drawing.Size(99, 22);
+            this.Xn.TabIndex = 0;
+            this.Xn.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Xk
+            // 
+            this.Xk.Location = new System.Drawing.Point(274, 98);
+            this.Xk.Name = "Xk";
+            this.Xk.Size = new System.Drawing.Size(100, 22);
+            this.Xk.TabIndex = 1;
+            // 
+            // a
+            // 
+            this.a.Location = new System.Drawing.Point(275, 143);
+            this.a.Name = "a";
+            this.a.Size = new System.Drawing.Size(100, 22);
+            this.a.TabIndex = 2;
+            // 
+            // b
+            // 
+            this.b.Location = new System.Drawing.Point(275, 194);
+            this.b.Name = "b";
+            this.b.Size = new System.Drawing.Size(100, 22);
+            this.b.TabIndex = 3;
+            // 
+            // DX
+            // 
+            this.DX.Location = new System.Drawing.Point(275, 240);
+            this.DX.Name = "DX";
+            this.DX.Size = new System.Drawing.Size(100, 22);
+            this.DX.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(247, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Xn";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(246, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Xk";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(246, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "a";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(247, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "b";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(246, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "DX";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(418, 146);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(370, 292);
+            this.dataGridView.TabIndex = 11;
+            // 
+            // Res
+            // 
+            this.Res.Location = new System.Drawing.Point(501, 59);
+            this.Res.Name = "Res";
+            this.Res.Size = new System.Drawing.Size(108, 38);
+            this.Res.TabIndex = 12;
+            this.Res.Text = "Res";
+            this.Res.UseVisualStyleBackColor = true;
+            this.Res.Click += new System.EventHandler(this.Res_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Res);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DX);
+            this.Controls.Add(this.b);
+            this.Controls.Add(this.a);
+            this.Controls.Add(this.Xk);
+            this.Controls.Add(this.Xn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Xn;
+        private System.Windows.Forms.TextBox Xk;
+        private System.Windows.Forms.TextBox a;
+        private System.Windows.Forms.TextBox b;
+        private System.Windows.Forms.TextBox DX;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button Res;
+    }
+}
+
---
+++ 4_лаба_Виндовс_Формам/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _4_лаба_Виндовс_Формам
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView.Columns.Add("ColumnX", "x");
+            dataGridView.Columns.Add("ColumnZ", "z");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Res_Click(object sender, EventArgs e)
+        {
+            // Получаем значения начального x, конечного x, параметров a и b, а также шага Delta x
+            if (double.TryParse(Xn.Text, out double xn) &&
+                double.TryParse(Xk.Text, out double xk) &&
+                double.TryParse(a.Text, out double aVal) &&
+                double.TryParse(b.Text, out double bVal) &&
+                double.TryParse(DX.Text, out double dx))
+            {
+                // Очищаем DataGridView перед заполнением новыми данными
+                dataGridView.Rows.Clear();
+
+                // Вычисляем и выводим значения функции для каждого x
+                for (double x = xn; x <= xk; x += dx)
+                {
+                    double z;
+                    if (x <= aVal)
+                    {
+                        z = Math.Pow(x, 2) + Math.Sin(x);
+                    }
+                    else if (x < bVal)
+                    {
+                        z = Math.Cos(Math.Pow(x, 2));
+                    }
+                    else
+                    {
+                        z = Math.Tan(x);
+                    }
+
+                    // Добавляем строку в DataGridView с результатом вычислений
+                    dataGridView.Rows.Add(x, z.ToString("F2"));
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ошибка ввода данных. Проверьте введенные значения.");
+            }
+        }
+    }
+}
